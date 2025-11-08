df49ca1e21decfb6d7e85bd0b0262a5175c847c0
MaxLoblad <mkala2025@agruni.edu.ge>
2025-11-09 02:21:05 +0400

    Delete homework-1 directory

--- homework-1/2.py
+++
@@ -1,7 +0,0 @@
-
-number = int(input("Enter natural number not greater than 1000: "))
-i = 0
-while i < number:
-    i=i+1
-    if (number % i) == 0:
-        print(i, end=" ")
--- homework-1/3.py
+++
@@ -1,8 +0,0 @@
-
-n = int(input("Enter natural number not greater than 100: "))
-i = 1
-fibonacci = [0, 1]
-while i < n-1:
-    fibonacci.append(fibonacci[i-1] + fibonacci[i])
-    i = i + 1
-print(fibonacci)
